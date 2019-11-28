395a4bf2521cf65fb60ac036ddcc1c4a8686440f
sumanth232 <sumanth232@gmail.com>
2019-11-28 13:56:52 -0500

    working code with go graph objects rendered on ui

--- mysite/safepath/urls.py
+++ mysite/safepath/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import SimpleCandlestickWithPandas
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('dashboard', SimpleCandlestickWithPandas.as_view(),name='simple-candlestick'),
 ]
--- mysite/safepath/views.py
+++ mysite/safepath/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from . import plots
+
+class SimpleCandlestickWithPandas(TemplateView):
+    template_name = 'plot.html'
+    def get_context_data(self, **kwargs):
+        context = super(SimpleCandlestickWithPandas, self).get_context_data(**kwargs)
+        context['3dplot'] = plots.get_topographical_3D_surface_plot()
+        context['piechart'] = plots.pie_chart()
+        return context
 
 
 def index(request):
